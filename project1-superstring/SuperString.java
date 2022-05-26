5ec50d874f130cdc73090bb31f9a490d68e5d674
mroam <1194289+mroam@users.noreply.github.com>
2022-05-26 14:32:26 -0400

    TextyText is now SuperString
    and has stub functions and tests for (get & chop) first (chord & lyric)

--- project1-superstring/SuperString.java
+++ project1-superstring/SuperString.java
@@ -1,4 +1,5 @@
 // import javafx.scene.control.*;    // for TextInputDialog
+// Extra info available at https://chordseasy.com/song/33119/never-gonna-give-you-up
 
 import javax.swing.*;
 import java.io.BufferedReader;
@@ -10,27 +11,29 @@ import java.util.ArrayList;
 /**
  * A String with some special tricks to let us practice.
  * Wants to have the "line i", "word j", and "char k" methods of LiveCode.
+ * Alternative approach to consider: make a string manipulator that receives Strings.
  *
  * @author Mike Roam
- * @version 2022e.May18
+ * @version 2022e.May26
  */
-public class TextyText {
+public class SuperString {
     private String myText = null;  // can be multi-line, e.g.  "lineOne\nlineTwo" beware OBOB!
 
-    private ArrayList<String> myLines = new ArrayList<String>();
+    private ArrayList<String> myLines = new ArrayList<String>(); // Used for cache; 
+    // what if we only use it when a getLine() is called? Do we need one for chars & words?
+    // Is this premature optimization?
+    // needs to import java.util.ArrayList
     // was: private String[] myLines = null; // mirrors myText, gets changed whenever myText changes!
 
-    // maybe a java.util.ArrayList<String> would be better?
-
     /**
-     * Constructor for objects of class TextyText.
+     * Constructor for objects of class SuperString.
      * This constructor without arguments uses a placeholder "empty text"
      */
-    public TextyText() {
+    public SuperString() {
         myText = "empty text";
         myLines.add("empty text");
         /* alternative: array String[]  instead of ArrayList<String>
-           myLines = new String[]{"empty text"}; */
+        myLines = new String[]{"empty text"}; */
         /* Note: various array initializations:
         String[] myStringArray = new String[3];
         String[] myStringArray = {"a", "b", "c"};
@@ -39,9 +42,9 @@ public class TextyText {
     }
 
     /**
-     * Constructor for objects of class TextyText
+     * Constructor for objects of class SuperString
      */
-    public TextyText(String newText) {
+    public SuperString(String newText) {
         myText = newText;
         this.splitIntoLines();
     }
@@ -77,14 +80,14 @@ public class TextyText {
 
     /**
      * based upon code in
-     <a href="https://examples.javacodegeeks.com/desktop-java/javafx/dialog-javafx/javafx-dialog-example/">...</a>
-     and
-     <a href="https://www.geeksforgeeks.org/javafx-textinputdialog/">...</a>
-     and reference
-     <a href="https://docs.oracle.com/javase/8/javafx/api/javafx/scene/control/TextInputDialog.html">...</a>
+    <a href="https://examples.javacodegeeks.com/desktop-java/javafx/dialog-javafx/javafx-dialog-example/">...</a>
+    and
+    <a href="https://www.geeksforgeeks.org/javafx-textinputdialog/">...</a>
+    and reference
+    <a href="https://docs.oracle.com/javase/8/javafx/api/javafx/scene/control/TextInputDialog.html">...</a>
 
-     had to
-     import javafx.scene.control.*
+    had to
+    import javafx.scene.control.*
      */
     public void askJavaFXUserForText() {
         /* Don't run this yet, gets "Error: uninitialized .."
@@ -99,11 +102,11 @@ public class TextyText {
         String entered = "none.";
 
         if (result.isPresent() ) {
-            entered = result.get();
+        entered = result.get();
         }
         // manipulates the Dialog: actionStatus.setText("Text entered: " + entered);
         this.setText( entered );
-        */
+         */
     }
 
     /**
@@ -119,7 +122,7 @@ public class TextyText {
             // StringBuffer allTheLines = new StringBuffer();
             ArrayList<String> allTheLines = new ArrayList<>();
             while ((currLine = reader.readLine()) != null)
-                // System.out.println(currLine);
+            // System.out.println(currLine);
                 allTheLines.add(currLine);
             myText = allTheLines.toString();  // Is this crazy?
             myText = String.join("/n", allTheLines);
@@ -131,11 +134,11 @@ public class TextyText {
         /* Here's an alternative that allows some parsing, from
         https://stackoverflow.com/questions/37769481/javafx-gui-that-opens-a-text-file-how-to-read-whats-in-text-file-and-edit-save
         try (Scanner scanner = new Scanner(new File("file.txt"))) {
-            while (scanner.hasNext())
-                System.out.println(scanner.next());
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+        while (scanner.hasNext())
+        System.out.println(scanner.next());
+        } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        }
         }
         Note: https://prutor.ai/difference-between-scanner-and-bufferreader-class-in-java/
         says,
@@ -151,27 +154,95 @@ public class TextyText {
      * returns the (y+1)th word of our text. 
      * "word" is anything between spaces. What about dog-food"
      * Eg "hi there".word(0) returns "hi"
+     * 
+     * How to do? Hint: check getLine( ) and splitIntoLines( )
      */
     public String getWord(int y)
     {
         System.out.println("getChar() doesn't work yet");
         return "yoiks";
     }
-    
-    
+
     /**
-     * returns the (y+1)th char of our text. 
+     * returns the (y+1)th char of our text.  Watch and test for OBOBs!
      * "char" is any character. Hmmm, what about unicode?
      * Eg "hi there".char(1) returns "h"
+     * Note: I'm using String rather than char or Character, so I can use my 
+    fake wrongslash /n linebreaks!
+     * Hint: See String in API:
+     * <a href="https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/String.html"
+    https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/String.html</a>
+     * Hint: check String.charAt(int i)    which also work for unicode, see CodePointAt for details
      */
-    public String getChar(int y)
+    public String getChar(int i)
     {
         // put your code here
         System.out.println("getChar() doesn't work yet");
         return "m";
     }
-    
-    
+
+    /**
+     * find chord i, starting count at ONE so watch & test for OBOBs.
+     */
+    public String getChord(int i) {
+        // put your code here
+        System.out.println("getChord(i) doesn't work yet");
+        return "{Gm}";
+    }
+
+    /**
+     * Would we want to number the chords? 
+     * If so, this could (when written) find the char offset into the string of chord number I
+     * Or not bother, instead while processing just keep pulling off first lyrics & first chord?
+     * If we cached all chords AND their locations we could later get their locations again,
+     * but does anybody need that?
+     */
+    public int indexOfChord(int i) {
+        //can say "String.indexOf( "{Gm}") but hoow do we find loc of second or third Gm?
+        System.out.println("indexOfChord(i) doesn't work yet");
+        return 5;
+    }
+
+    /**
+     * Should be "" if current info is starting with a chord?
+     * Could alternatively be a static method which both receives and returns Strings. (Receive SuperString??)
+     */    
+    String getFirstLyric( ) {
+        System.out.println("getFirstLyric( ) doesn't work yet");
+        return "Temp";
+    }
+
+    /**
+     * Handy song manipulation, does NOT mess with this.myText( )
+     * Could alternatively be a static method which both receives and returns Strings. (Receive SuperString??)
+     */
+    String chopFirstLyric( ) {
+        String after = null;
+        System.out.println("chopFirstLyric( ) doesn't work yet");
+        after = "Temp";
+        return after;
+    }
+
+    /**
+     * Should be "" if current info is starting with a lyric?
+     * Might return "{F G Em Am}  or {F}
+     * Could alternatively be a static method which both receives and returns Strings. (Receive SuperString??)
+     */    
+    String getFirstChord( ) {
+        System.out.println("getFirstChord( ) doesn't work yet");
+        return "Temp";
+    }
+
+    /**
+     * Handy song manipulation, does NOT mess with this.myText( )
+     * Could alternatively be a static method which both receives and returns Strings. (Receive SuperString??)
+     */
+    String chopFirstChord( ) {
+        String after = null;
+        System.out.println("chopFirstLyric( ) doesn't work yet");
+        after = "Temp";
+        return after;
+    }
 
     /**
      * Returns one of the lines of myText.
@@ -187,7 +258,7 @@ public class TextyText {
             return "";  // throw new 
         }
         if (wantedLineNum > myLines.size() ) {   /* Obob?? How could we tell? :) */
-             /* No parentheses on length when measuring an array[].length, 
+            /* No parentheses on length when measuring an array[].length, 
              * but use parentheses when measuring String.length( )! */
             return "";
         }
@@ -207,13 +278,13 @@ public class TextyText {
         String[] theLines = myText.split( "/n" );
         /* if myLines were merely array of String, we'd be done. */
         /* The trick in the next line is the cast. Oops, compiles but doesn't work. See
-        * https://beginnersbook.com/2015/05/java-string-to-arraylist-conversion/ */
+         * https://beginnersbook.com/2015/05/java-string-to-arraylist-conversion/ */
         // myLines = (ArrayList<String>) Arrays.asList( theLines );
         myLines.clear( );  // faster than ArrayList.removeAll() but leaves nulls.
         for (String aLine : theLines) {
-        //for (int i = 0; i < theLines.size(); i++){
-             myLines.add(aLine);   // not "append!" 
+            //for (int i = 0; i < theLines.size(); i++){
+            myLines.add(aLine);   // not "append!" 
         }
     }
 
-} // class TextyText
+} // class SuperString
--- project1-superstring/SuperStringTest.java
+++ project1-superstring/SuperStringTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * The test class TextyTextTest.
+ * The test class SuperStringTest.
  *
  * @author  Mike Roam
  * @version 2022 May 19
  */
-public class TextyTextTest
+public class SuperStringTest
 {
     /**
-     * Default constructor for test class TextyTextTest
+     * Default constructor for test class SuperStringTest
      */
-    public TextyTextTest()
+    public SuperStringTest()
     {
     }
 
@@ -42,22 +42,60 @@ public class TextyTextTest
     @Test
     public void getWord2()
     {
-        TextyText textyTex1 = new TextyText("Instructions for living a life:\nPay attention.\nBe astonished.\nTell about it.\n --Mary Oliver");
+        SuperString textyTex1 = new SuperString("Instructions for living a life:\nPay attention.\nBe astonished.\nTell about it.\n --Mary Oliver");
         assertEquals("for", textyTex1.getWord(2));
     }
 
     @Test
     public void getWord2yoiks()
     {
-        TextyText textyTex1 = new TextyText("Instructions for living a life:\nPay attention.\nBe astonished.\nTell about it.\n --Mary Oliver");
-        assertEquals("yoi" + "ks", textyTex1.getWord(2));
+        SuperString supstr = new SuperString("Instructions for living a life:\nPay attention.\nBe astonished.\nTell about it.\n --Mary Oliver");
+        assertEquals("yoi" + "ks", supstr.getWord(2));
     }
 
+    @Test
+    public void getChord()
+    {
+        SuperString supstr = new SuperString("{F G Em Am} /n {F}We're no strangers to {G}love.");
+        assertEquals("{F}", supstr.getChord( 1 ));
+    }
+    
+    @Test
+    public void getFirstChord( )
+    {
+        SuperString supstr = new SuperString("{F}You know the rules and {G}so do I");
+        assertEquals("{F}", supstr.getFirstChord( ));
+    }
+    
+    @Test
+    public void chopFirstChord( )
+    {
+        SuperString supstr = new SuperString("{F}A full commitment's what I'm {G}thinking of");
+        String leftovers = supstr.chopFirstChord( );
+        assertEquals("A full commitment's what I'm {G}thinking of", leftovers );
+    }
+    
+    @Test
+    public void getFirstLyric( )
+    {
+        SuperString supstr = new SuperString("Never gonna {F}give you {G}up, Never gonna {Em}let you {Am}down");
+        // interesting: keep the space after "gonna" or NOT? What if it were deliberately two spaces??
+        assertEquals("Never gonna", supstr.getFirstLyric( ) );
+    }
+    
+    @Test
+    public void chopFirstLyric( )
+    {
+        SuperString supstr = new SuperString("Never gonna r{F}un a{G}round and d{E}esert{Am} you");
+        String leftovers = supstr.chopFirstLyric( );
+        assertEquals("{F}un a{G}round and d{E}esert{Am} you", leftovers );
+    }
+    
     /*     * * * Silly to test this unless we know what the user is going to insert. Need to mock up!
     @Test
     public void userText()
     {
-    TextyText banjo = new TextyText();
+    SuperString banjo = new SuperString();
     banjo.askUserForText();   // I put in by hand: (with quotes) "sometimes\nmelancholy leaves me breathless"
     assertEquals("sometimes\nmelancholy leaves me breathless", banjo.getText());
     }
@@ -66,9 +104,9 @@ public class TextyTextTest
     /**
      * Trying to test getLine(x).
      * Edge cases: 
-     * [ ] null TextyText
+     * [ ] null SuperString
      * [ ] using non-arg constructor (which makes "<empty text>"
-     * [ ] empty TextyText
+     * [ ] empty SuperString
      * [ ] index out of bounds
      */
     @Test
@@ -80,14 +118,14 @@ public class TextyTextTest
          * IF we also have a static getLine( TextText myStr, int i )  THEN we could notice null
          * and return "".
          */
-        TextyText txtOne = null; 
+        SuperString txtOne = null; 
         // assertEquals("", txtOne.getLine(-1) );
         // assertEquals("", txtOne.getLine(0) );
         // assertEquals("", txtOne.getLine(1) );
         // assertEquals("", txtOne.getLine(2) );
 
         // let's try empty but constructed. (non-arg constructor makes "<empty text>" !)
-        txtOne = new TextyText( ); // can it survive?
+        txtOne = new SuperString( ); // can it survive?
         assertEquals("", txtOne.getLine(-1) );
         assertEquals("", txtOne.getLine(0) );
         assertEquals("empty text", txtOne.getLine(1) );
@@ -130,7 +168,7 @@ public class TextyTextTest
         assertEquals("", txtOne.getLine(3));
 
         // three liner
-        txtOne = new TextyText("line1/nline2/nline3");
+        txtOne = new SuperString("line1/nline2/nline3");
         assertEquals("line1/nline2/nline3", txtOne.getText());
         assertEquals("", txtOne.getLine(0));
         assertEquals("line1", txtOne.getLine(1));
@@ -155,7 +193,7 @@ public class TextyTextTest
     @Test
     public void getWord()
     {
-        TextyText txtOne = new TextyText("word1 word2  word.3");
+        SuperString txtOne = new SuperString("word1 word2  word.3");
         assertEquals("", txtOne.getWord(-1));
         assertEquals("", txtOne.getWord(0));
         assertEquals("wordl", txtOne.getWord(1));
@@ -171,13 +209,13 @@ public class TextyTextTest
     @Test
     public void getChar()
     {
-        TextyText txtOne = new TextyText("line1");
+        SuperString txtOne = new SuperString("line1");
         assertEquals("", txtOne.getChar(-1));
         assertEquals("", txtOne.getChar(0));
         assertEquals("l", txtOne.getChar(1));
         assertEquals("i", txtOne.getChar(2));
         assertEquals("", txtOne.getChar(10));
     }
-} // class TextyTextTest
+} // class SuperStringTest
 
 
