3f63ad1db0ccfe0d5876971ed9468ce3745c9b5e
mroam <1194289+mroam@users.noreply.github.com>
2022-05-27 20:42:49 -0400

    hello new pix, bye old IDEA starters

--- IDEA-SuperString/not-add-sample-code/src/com/michaelroam/hellomilton/HelloMIlton.java
+++
@@ -1,8 +0,0 @@
-package com.michaelroam.hellomilton;
-
-public class HelloMIlton {
-    public static void main(String[] args) {
-        System.out.println("Hello, Milton!");
-        System.out.println("Wind in my heart...");
-    }
-}
